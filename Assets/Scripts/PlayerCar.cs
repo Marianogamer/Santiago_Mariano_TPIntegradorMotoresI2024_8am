d857dbfd86d0cc113fc4c3d188de2e5d58aaf9a7
Marianogamer <marianogamer2473@gmail.com>
2024-11-01 00:28:40 -0300

    2

--- Assets/Scripts/PlayerCar.cs
+++ Assets/Scripts/PlayerCar.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class PlayerCar : MonoBehaviour
 {
+    [SerializeField] float turnSpeed = 5;
+    Quaternion targetRotation;
+
+    void Update()
+    {
+        SetRotationPoint();
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+    }
     private void SetRotationPoint()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Plane plane = new Plane(Vector3.up, Vector3.zero);
+        float distance;
+        if(plane.Raycast(ray, out distance))
+        {
+            Vector3 target = ray.GetPoint(distance);
+            Vector3 direction = target - transform.position;
+            float rotationAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            targetRotation = Quaternion.Euler(0, rotationAngle, 0);
+        }
     }
 }
